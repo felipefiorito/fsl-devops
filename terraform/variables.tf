a0163a81ced7280e50294f3dec9d6afac780f598
Fellipe <felipefiorito@gmail.com>
2026-05-06 16:04:24 -0300

    tf commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,20 @@
 # In this file put the variables related to the deployment
-variable "variable_name" {
-    type = "type",
-    description = "Description"
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "aws_account_id" {
+    type = string
+    default = "992382773369"
+}
+
+variable "environment" {
+    type = string
+    description = "environment to dpeloy, devel or stage"
+}
+
+variable "project_prefix" {
+    type = string
+    default = "fsl-devops-assessment"
 }
